561b01a49531ff387ae79f4e5fc2a36ad11e8803
GitAdamReed <adamreed170@gmail.com>
2025-12-13 01:21:48 +0000

    Day 1 Part 1 complete

--- AdventOfCode2025/AdventOfCode2025/Classes/Day.cs
+++ AdventOfCode2025/AdventOfCode2025/Classes/Day.cs
@@ -8,12 +8,28 @@ namespace AdventOfCode2025.Classes
 {
     public abstract class Day
     {
-        private const string _inputPath = "C:\\Users\\adamr\\Documents\\GitHub\\Advent_Of_Code\\advent-of-code-2025\\AdventOfCode2025\\AdventOfCode2025\\Input";
-        private const string _outputPath = "C:\\Users\\adamr\\Documents\\GitHub\\Advent_Of_Code\\advent-of-code-2025\\AdventOfCode2025\\AdventOfCode2025\\Output";
-        
+        private const string _inputPath = "C:\\Users\\adamr\\Documents\\GitHub\\Advent_Of_Code\\advent-of-code-2025\\AdventOfCode2025\\AdventOfCode2025\\Input\\";
+        private const string _outputPath = "C:\\Users\\adamr\\Documents\\GitHub\\Advent_Of_Code\\advent-of-code-2025\\AdventOfCode2025\\AdventOfCode2025\\Output\\";
+
         protected string InputPath { get; } = _inputPath;
         protected string OutputPath { get; } = _outputPath;
 
+        public Day()
+        {
+            CreateOutput();
+        }
+
+        public override string ToString()
+        {
+            return GetType().Name;
+        }
+
+        protected string GetTextFilePath(string type)
+        {
+            if (type == "input") return $"{_inputPath}{this}.txt";
+            else if (type == "output") return $"{_outputPath}{this}.txt";
+            else throw new ArgumentException("Invalid text file type.");
+        }
         protected abstract void CreateOutput();
     }
 }
--- AdventOfCode2025/AdventOfCode2025/Classes/Day1.cs
+++ AdventOfCode2025/AdventOfCode2025/Classes/Day1.cs
@@ -10,7 +10,28 @@ namespace AdventOfCode2025.Classes
     {
         protected override void CreateOutput()
         {
-            throw new NotImplementedException();
+            File.WriteAllText(GetTextFilePath("output"), $"Part 1: {CrackCode()}");
+        }
+
+        private int CrackCode()
+        {
+            int currNum = 50;
+            int zeroCount = 0;
+            string[] instructions = File.ReadAllLines(GetTextFilePath("input"));
+
+            foreach (var i in instructions)
+            {
+                int delta = Int32.Parse(i[1..]) % 100; // Same as Int32.Parse(i.Substring(1) % 100)
+                if (i[0] == 'L') currNum -= delta;
+                else currNum += delta;
+
+                if (currNum > 99) currNum -= 100;
+                else if (currNum < 0) currNum += 100;
+
+                if (currNum == 0) zeroCount++;
+            }
+
+            return zeroCount;
         }
     }
 }
