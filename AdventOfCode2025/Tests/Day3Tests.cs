7746564a79e7633e957f20f17ad2f022eb4a079c
GitAdamReed <adamreed170@gmail.com>
2025-12-16 23:21:15 +0000

    Add Tests project

---
+++ AdventOfCode2025/Tests/Day3Tests.cs
@@ -0,0 +1,16 @@
+﻿using AdventOfCode2025.Classes;
+
+namespace Tests
+{
+    public class Day3Tests
+    {
+        [TestCase("987654321111111", 98)]
+        [TestCase("811111111111119", 89)]
+        [TestCase("234234234234278", 78)]
+        [TestCase("818181911112111", 92)]
+        public void FindJoltagePassesExamples(string example, int expectedResult)
+        {
+            Assert.That(Day3.FindJoltage(example), Is.EqualTo(expectedResult));
+        }
+    }
+}
